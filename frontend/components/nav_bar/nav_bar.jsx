ae8959760706b5840efb0b32ca3eb7a79141aca5
HN <hhnguyen92@gmail.com>
2018-06-07 10:02:09 -0700

    Style css whether user exists or not homepage with navbar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = ({currentUser, logout}) => {
+const NavBar = (props) => {
   
   const logged = () => {
     return (
@@ -13,10 +13,10 @@ const NavBar = ({currentUser, logout}) => {
             
             <img className="user-avatar" src="https://cdn1.iconfinder.com/data/icons/circle-outlines/512/User_Account_Avatar_Person_Profile_Login_Human-512.png" />
             {/* credit:https://www.iconfinder.com/icons/640974/account_avatar_human_login_person_profile_user_icon */}
-          <Link className="nav-bar-link"to={`/users/${currentUser.id}`}>
-            {currentUser.first_name}
+          <Link className="nav-bar-link"to={`/users/${props.currentUser.id}`}>
+            {props.currentUser.first_name}
           </Link>
-          <button className="nav-bar-link" onClick={() => logout()}>Logout</button>
+          <button className="nav-bar-link" onClick={() => props.logout()}>Logout</button>
             <Link className="nav-bar-link" to='/events/new'>Create Event</Link>
           </section>
         </div>
@@ -26,21 +26,27 @@ const NavBar = ({currentUser, logout}) => {
     );
   };
 
+
+
   const newUser = () => {
     return (
       <div>
-        <Link to='/'>EventsNow</Link>
-        <section>
-        <Link to='/events'>Browse Events</Link>
-        <Link to='/signin'>Sign In</Link>
-        <Link to='/events/new'>Create Event</Link>
-        </section>
+        <div className="nav-bar-main-div">
+          <Link className="nav-bar-home" to='/'>EventsNow</Link>
+          <section className="nav-bar-right-side">
+          <Link className="nav-bar-link" to='/events'>Browse Events</Link>
+            <Link className="nav-bar-link" to='/signin'>Sign In</Link>
+            <Link className="nav-bar-link" to='/events/new'>Create Event</Link>
+          </section>
+        </div>
+          <img className="background-banner" src="http://staybarcelonaapartments.com/blog/wp-content/uploads/2017/11/the-color-run.jpg" />
+          {/* credit:http://staybarcelonaapartments.com/blog/whats-hot-in-barcelona/color-run-skittles-barcelona/ */}
       </div>
     );
   };
 
 
-  return currentUser ? logged() : newUser();
+  return props.currentUser ? logged() : newUser();
 
 };
 // const NavBar = ({ currentUser, logout }) => {
