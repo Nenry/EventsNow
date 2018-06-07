8190f7d4d17b622e1c3f18fed03029a4deb3045c
HN <hhnguyen92@gmail.com>
2018-06-07 15:32:14 -0700

    Modify homebg to be home component and add front banner

--- frontend/components/background/homebg_img.jsx
+++ frontend/components/background/homebg_img.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 
 
 const homeBgImg = () =>(
-  <img className="background-banner" src="http://staybarcelonaapartments.com/blog/wp-content/uploads/2017/11/the-color-run.jpg" />
+  <div>
+    <img className="background-banner" src="http://staybarcelonaapartments.com/blog/wp-content/uploads/2017/11/the-color-run.jpg" />
+    <div className="home-flex">
+      <div className="home-message">
+        Find your next experience
+      </div>
+
+    </div>
+
+    <div className="temp-events">
+        Events coming soon!
+    </div>
+    
+
+    </div>
+  
+
 );
 
 export default homeBgImg;
+
+
