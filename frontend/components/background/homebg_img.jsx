9a14da8cab13423f2d182d6d12363e8f4d1ac506
HN <hhnguyen92@gmail.com>
2018-06-07 22:21:00 -0700

    Change home page background image

--- frontend/components/background/homebg_img.jsx
+++ frontend/components/background/homebg_img.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 
 const homeBgImg = () =>(
   <div className="home-bg-img">
-    <img className="background-banner" src="http://staybarcelonaapartments.com/blog/wp-content/uploads/2017/11/the-color-run.jpg" />
+    <img className="background-banner" src="https://farm2.staticflickr.com/1759/28789455328_ed8f3a14bd_h.jpg" />
+    {/* img credit: https://500px.com/krunja */}
     <div className="home-flex">
       <div className="home-message">
         Find your next experience
