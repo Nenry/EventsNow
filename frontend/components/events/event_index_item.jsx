d93237fd0cd5ed5995589f86dafcaf0ab87794f2
HN <hhnguyen92@gmail.com>
2018-06-09 16:47:09 -0700

    Made events on homepage grid display

--- frontend/components/events/event_index_item.jsx
+++ frontend/components/events/event_index_item.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const EventIndexItem = props => (
   <div>
-    {props.event.title}
     <img src={props.event.img_url} />
+    {props.event.date}
+    {props.event.time_start}
+    {props.event.title}
 
   </div>
 );
