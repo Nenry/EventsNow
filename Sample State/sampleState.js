5700108cf696d50fa21e3241b5cd3d7bbe06bae9
HN <hhnguyen92@gmail.com>
2018-07-18 09:55:21 -0700

    Change background and event index img size to cover instead of 100%

--- Sample State/sampleState.js
+++ Sample State/sampleState.js
@@ -1,72 +1,72 @@
-{
- entities: {
-    events: {
-      1: {
-        id: 1,
-        title: "title1"
-        body: "description",
-        host_id : 5,
-        date: '5/1/2001',
-        address: "123 sesame way...",
-        city: "San Francisco",
-        state: "CA",
-        categoryId: 1,
-        ticketsTotal: 500,
-        price: 0
-        imgUrl: 'flicker.com/img/2'
-      },
-      2: {
-        id: 2,
-        title: "title2"
-        body: "description2",
-        host_id: 28,
-        date: 10/2/3005,
-        location: "543 open way...",
-        city: "Oakland",
-        state: "CA",
-        categoryId: 5,
-        ticketsTotal: 400,
-        price: 20
-        imgUrl: 'flicker.com/img/1'
-      }
-    },
-  tickets: {
-      1 : {
-        id: 1,
-        event_id: 2,
-        type: 'profit'/'free'/'donation',
-        buyer_id: 7
-      }
-    },
-    users: {
-      1: {
-        id: 1,
-        host_events_ids: [1, 5, 7],
-        attend_event_ids: [2, 3, 8]
-        email: "SquirtleSquad@gmail.com"
-        bookmark_ids: [7, 3, 1]
-      }
-    },
-    category: {
-      1: {
-          id: 1,
-          title: 'free'
-      }
-      2: {
-         id: 2
-         title: 'music'
-      }
-    }
-  },
-  session: {
-    currentUserId: 25
-  },
-  ui: {
-    loading: true/false
-  }
-  errors: {
-    login: ["Credentials do not match"],
-    signUp: ['Password is too short']
-    eventForm: ["Title cannot be blank"]
-  }
-}
+// {
+//  entities: {
+//     events: {
+//       1: {
+//         id: 1,
+//         title: "title1"
+//         body: "description",
+//         host_id : 5,
+//         date: '5/1/2001',
+//         address: "123 sesame way...",
+//         city: "San Francisco",
+//         state: "CA",
+//         categoryId: 1,
+//         ticketsTotal: 500,
+//         price: 0
+//         imgUrl: 'flicker.com/img/2'
+//       },
+//       2: {
+//         id: 2,
+//         title: "title2"
+//         body: "description2",
+//         host_id: 28,
+//         date: 10/2/3005,
+//         location: "543 open way...",
+//         city: "Oakland",
+//         state: "CA",
+//         categoryId: 5,
+//         ticketsTotal: 400,
+//         price: 20
+//         imgUrl: 'flicker.com/img/1'
+//       }
+//     },
+//   tickets: {
+//       1 : {
+//         id: 1,
+//         event_id: 2,
+//         type: 'profit'/'free'/'donation',
+//         buyer_id: 7
+//       }
+//     },
+//     users: {
+//       1: {
+//         id: 1,
+//         host_events_ids: [1, 5, 7],
+//         attend_event_ids: [2, 3, 8]
+//         email: "SquirtleSquad@gmail.com"
+//         bookmark_ids: [7, 3, 1]
+//       }
+//     },
+//     category: {
+//       1: {
+//           id: 1,
+//           title: 'free'
+//       }
+//       2: {
+//          id: 2
+//          title: 'music'
+//       }
+//     }
+//   },
+//   session: {
+//     currentUserId: 25
+//   },
+//   ui: {
+//     loading: true/false
+//   }
+//   errors: {
+//     login: ["Credentials do not match"],
+//     signUp: ['Password is too short']
+//     eventForm: ["Title cannot be blank"]
+//   }
+// }
